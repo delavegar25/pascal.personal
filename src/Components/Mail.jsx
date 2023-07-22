42e672b8ff295e0990bb9576ebddeaad34aa84c2
Pascal <pascalokereke18@gmail.com>
2023-07-22 17:28:17 +0100

    mail mail

--- src/Components/Mail.jsx
+++ src/Components/Mail.jsx
@@ -70,7 +70,25 @@ const Mail = () => {
 //         message: toSend.message,
 //       }, publicKey);
 
+ 
+  const emailData = {
+  to_email: 'recipient@example.com',
+  from_email: 'sender@example.com',
+  subject: 'Test Email',
+  message: 'This is a test email sent using Email.js',
+}
+};
 
+  emailjs.send(
+  emailjsServiceId,
+  emailjsTemplateId,
+)
+.then((response) => {
+console.log('Email sent successfully', response);
+})
+.catch((error) => {
+console.error('Failed to send email', error);
+}); 
 
       error.current.style.display = 'none'
       reset();
@@ -78,27 +96,7 @@ const Mail = () => {
   }
 
 
-  const handleChange = (e) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
 
-    const emailData = {
-      to_email: 'recipient@example.com',
-      from_email: 'sender@example.com',
-      subject: 'Test Email',
-      message: 'This is a test email sent using Email.js',
-    }
-  };
-
-  emailjs.send(
-    emailjsServiceId,
-    emailjsTemplateId,
-  )
-  .then((response) => {
-    console.log('Email sent successfully', response);
-  })
-  .catch((error) => {
-    console.error('Failed to send email', error);
-  });
 
   useEffect(() => {
     const arrayOfSub = ['work', 'chat', 'collaboration']
