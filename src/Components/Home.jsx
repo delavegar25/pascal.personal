6e3dedeb7958e008af08f52f2dab0b200eb9e08d
Pascal <pascalokereke18@gmail.com>
2023-07-22 12:27:04 +0100

    resume upload

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
   const handleDownload = () => {
     saveAs(
       "google drive url",
+      "https://drive.google.com/file/d/1MnD0s5X9FnF22F-wa1Gept5hGKsZBPUq/view?usp=drive_link",
       "Resume.pdf"
     )
   }
